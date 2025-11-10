152b257d0fd7a6790d755771aee64333ce8bee14
Striatp <sebastien.works@outlook.com>
2025-11-10 11:08:44 +0100

    build(script): add a powershell script for building and testing 🛠

---
+++ scripts/build-test.ps1
@@ -0,0 +1,28 @@
+#!/usr/bin/env pwsh
+
+[System.String] $TestsDirectory = "./tests" ;
+[System.String] $BuildDirectory = "./bin" ;
+
+# Create build directory if non-existent
+if (-not (Test-Path $BuildDirectory)) {
+    New-Item -ItemType Directory -Path $BuildDirectory | Out-Null ;
+}
+
+Write-Host "Building test runner with Clang.";
+
+clang -Wall -Wextra -Werror "$TestsDirectory/run_tests.c" "$TestsDirectory/test_add.c" -o "$BuildDirectory/run_tests.exe" ;
+
+Write-Host "Build completed.";
+
+# Run tests
+Write-Host "Running tests...";
+
+& "$BuildDirectory/run_tests.exe"
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Some tests failed ❌" ;
+    exit 1 ;
+}
+
+Write-Host "All tests passed ✅" ;
+exit 0 ;
