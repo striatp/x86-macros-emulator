2615a0668272ece6a600e58e6be290881f22e1ca
Striatp <sebastien.works@outlook.com>
2025-11-10 09:32:23 +0100

    feat(include): export macros headers ✨

---
+++ source/cassembly.h
@@ -0,0 +1,11 @@
+#ifndef CASSEMBLY_H
+#define CASSEMBLY_H
+#pragma once
+
+#include "include/arithmetics.h"
+#include "include/bitwise.h"
+#include "include/data.h"
+#include "include/registries.h"
+#include "include/system.h"
+
+#endif /* CASSEMBLY_H */
