a41ede40f03dc79e2078d4bfbaad5761d8027fc2
Striatp <sebastien.works@outlook.com>
2025-11-10 09:29:21 +0100

    feat(include): add system types definition macros header ✨

---
+++ source/include/system.h
@@ -0,0 +1,22 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+#pragma once
+
+    #include <stdint.h>
+    #include <stdbool.h>
+
+    #define _INT int32_t
+    #define _UINT uint32_t
+    #define _INT16 int16_t
+    #define _UINT16 uint16_t
+    #define _BYTE uint8_t
+    #define _SBYTE int8_t
+    #define _LONG int64_t
+    #define _ULONG uint64_t
+
+    #define _VOID void
+    #define _BOOL bool
+    #define _TRUE true
+    #define _FALSE false
+
+#endif /* SYSTEM_H */
