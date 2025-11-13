f8a11ba39892d94c30cd8afc7248eaa74a1cf686
Striatp <sebastien.works@outlook.com>
2025-11-13 20:47:29 +0100

    feat(core): rework minimal example for macros ✨

--- source/main.c
+++ source/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-#include "cassembly.h"
+#include "x84-macros-emulator.h"
 
-int main(void) {
+int main() {
 
-    _MOV(EAX, 1);
-    _MOV(EBX, 2);
+    ADD(RAX, 0x80);
+    ADD(RAX, RDX);
+    ADD(AH, 0x45);
+    // ADD(RAX, EDX);
+    // ADD(EAX, RDX);
+    DEC(EAX);
+    MOV(RAX, 0x0);
+    DEC(RAX);
+    DEC(RAX);
 
-    _ADD(EAX, EBX);
+    SUB(RAX, 0xFFFFFFFF);
+    ADD(RAX, 0xF);
 
-    printf("%d, %d, %d\n", _ZF, _SF, _CF);
-    printf("%d", EAX); // outputs: 3
+    //_ADD(RAX, 5);
+    printf("RAX = 0x%016llX | CF=%d ZF=%d SF=%d OF=%d\n", RAX, _CF, _ZF, _SF, _OF);
 
     return 0;
 }
