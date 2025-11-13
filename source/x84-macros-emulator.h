17eaee0129851a908a57f34916360495ec31278b
Striatp <sebastien.works@outlook.com>
2025-11-13 20:48:24 +0100

    feat(include): rework exported control flow macros header ✨

---
+++ source/x84-macros-emulator.h
@@ -0,0 +1,12 @@
+#ifndef CASSEMBLY_H
+#define CASSEMBLY_H
+#pragma once
+
+#include "include/arithmetics.h"
+#include "include/bitwise.h"
+#include "include/control.h"
+#include "include/data.h"
+#include "include/registers.h"
+#include "include/system.h"
+
+#endif /* CASSEMBLY_H */
