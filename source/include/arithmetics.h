3e65996cd19891e0fba1658e19af120c0f58d672
Striatp <sebastien.works@outlook.com>
2025-11-13 20:37:57 +0100

    feat(include): rework logics and improve coherence ✨

--- source/include/arithmetics.h
+++ source/include/arithmetics.h
@@ -2,13 +2,30 @@
 #ifndef ARITHMETICS_H
 #define ARITHMETICS_H
 
-    #define _ADD(destination, source) do { \
-        _INT _T1 = (destination); \
-        _INT _T2 = (source); \
-        _INT _RES = _T1 + _T2; \
+    #include <stdint.h>
+
+    // Checks type compability
+    #define IS_INTEGER_LITERAL(x) ( \
+        __builtin_constant_p(x) && ( \
+            __builtin_types_compatible_p(__typeof__(x), int) || \
+            __builtin_types_compatible_p(__typeof__(x), long) || \
+            __builtin_types_compatible_p(__typeof__(x), long long) || \
+            __builtin_types_compatible_p(__typeof__(x), unsigned int) || \
+            __builtin_types_compatible_p(__typeof__(x), unsigned long) || \
+            __builtin_types_compatible_p(__typeof__(x), unsigned long long) ) )
+
+    // ADD instruction
+    #define ADD(destination, source) do { \
+        _Static_assert( \
+            __builtin_types_compatible_p(__typeof__(destination), __typeof__(source)) || \
+            IS_INTEGER_LITERAL(source), \
+            "destination and source must have the same type or source must be an integer literal" \
+        ); \
+        int64_t _T1 = (destination); \
+        int64_t _T2 = (source); \
+        int64_t _RES = _T1 + _T2; \
         (destination) = _RES; \
         \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
         _CF = ((unsigned)_T1 + (unsigned)_T2 < (unsigned)_T1); \
@@ -17,148 +34,130 @@
         _AF = (((_T1 ^ _T2 ^ _RES) & 0x10) != 0); \
     } while (0)
 
-    #define _SUB(destination, source) do { \
-        _INT _T1 = (destination); \
-        _INT _T2 = (source); \
-        _INT _RES = _T1 - _T2; \
+    // SUB instruction
+    #define SUB(destination, source) do { \
+        _Static_assert(__builtin_types_compatible_p(__typeof__(destination), __typeof__(source)) || \
+                       IS_INTEGER_LITERAL(source), \
+                       "destination and source must be the same type or source must be an integer literal"); \
+        int64_t _T1 = (destination); \
+        int64_t _T2 = (source); \
+        int64_t _RES = _T1 - _T2; \
         (destination) = _RES; \
         \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
-        _CF = ((unsigned)_T1 < (unsigned)_T2); \
+        _CF = ((uint64_t)_T1 < (uint64_t)_T2); \
         _OF = ((_T1 < 0 && _T2 > 0 && _RES > 0) || (_T1 > 0 && _T2 < 0 && _RES < 0)); \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = (((_T1 ^ _T2 ^ _RES) & 0x10) != 0); \
-    } while (0)
+    } while(0)
 
-    #define _MUL(destination, source) do { \
-        _INT _T1 = (destination); \
-        _INT _T2 = (source); \
-        _INT _RES = _T1 * _T2; \
+    // MUL instruction
+    #define MUL(destination, source) do { \
+        _Static_assert(__builtin_types_compatible_p(__typeof__(destination), __typeof__(source)) || \
+                       IS_INTEGER_LITERAL(source), \
+                       "destination and source must be the same type or source must be an integer literal"); \
+        int64_t _T1 = (destination); \
+        int64_t _T2 = (source); \
+        int64_t _RES = _T1 * _T2; \
         (destination) = _RES; \
         \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
         _CF = (_T1 != 0 && _RES / _T1 != _T2); \
         _OF = (_T1 != 0 && _RES / _T1 != _T2); \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = (((_T1 ^ _T2 ^ _RES) & 0x10) != 0); \
-    } while (0)
+    } while(0)
 
-    #define _IMUL(destination, source) do { \
-        _INT _T1 = (destination); \
-        _INT _T2 = (source); \
-        _INT _RES = _T1 * _T2; \
+    // DIV instruction
+    #define DIV(destination, source) do { \
+        _Static_assert(__builtin_types_compatible_p(__typeof__(destination), __typeof__(source)) || \
+                       IS_INTEGER_LITERAL(source), \
+                       "destination and source must be the same type or source must be an integer literal"); \
+        int64_t _T1 = (destination); \
+        int64_t _T2 = (source); \
+        int64_t _RES = (_T2 != 0) ? (_T1 / _T2) : 0; \
         (destination) = _RES; \
         \
-        /* Update flags */ \
-        _ZF = (_RES == 0); \
-        _SF = (_RES < 0); \
-        _CF = (_T1 != 0 && _RES / _T1 != _T2); \
-        _OF = (_T1 != 0 && _RES / _T1 != _T2); \
-        _PF = (__builtin_parity((unsigned)_RES) == 0); \
-        _AF = (((_T1 ^ _T2 ^ _RES) & 0x10) != 0); \
-    } while (0)
-
-    #define _DIV(destination, source) do { \
-        _INT _T1 = (destination); \
-        _INT _T2 = (source); \
-        _INT _RES = (_T2 != 0) ? (_T1 / _T2) : 0; \
-        (destination) = _RES; \
-        \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
         _CF = 0; \
         _OF = 0; \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = 0; \
-    } while (0)
+    } while(0)
 
-    #define _IDIV(destination, source) do { \
-        _INT _T1 = (destination); \
-        _INT _T2 = (source); \
-        _INT _RES = (_T2 != 0) ? (_T1 / _T2) : 0; \
+    // MOD instruction
+    #define MOD(destination, source) do { \
+        _Static_assert(__builtin_types_compatible_p(__typeof__(destination), __typeof__(source)) || \
+                       IS_INTEGER_LITERAL(source), \
+                       "destination and source must be the same type or source must be an integer literal"); \
+        int64_t _T1 = (destination); \
+        int64_t _T2 = (source); \
+        int64_t _RES = (_T2 != 0) ? (_T1 % _T2) : 0; \
         (destination) = _RES; \
         \
-        /* Update flags */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
         _CF = 0; \
         _OF = 0; \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = 0; \
-    } while (0)
+    } while(0)
 
-    #define _MOD(destination, source) do { \
-        _INT _T1 = (destination); \
-        _INT _T2 = (source); \
-        _INT _RES = (_T2 != 0) ? (_T1 % _T2) : 0; \
-        (destination) = _RES; \
-        \
-        /* Update flags register */ \
-        _ZF = (_RES == 0); \
-        _SF = (_RES < 0); \
-        _CF = 0; \
-        _OF = 0; \
-        _PF = (__builtin_parity((unsigned)_RES) == 0); \
-        _AF = 0; \
-    } while (0)
-
-    #define _NEG(registry) do { \
-        _INT _T = (registry); \
-        _INT _RES = -_T; \
+    // NEG instruction
+    #define NEG(registry) do { \
+        int64_t _T = (registry); \
+        int64_t _RES = -_T; \
         (registry) = _RES; \
         \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
         _CF = (_T != 0); \
-        _OF = (_T == _INT32_MIN); \
+        _OF = (_T == INT64_MIN); \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = ((_T ^ _RES) & 0x10) != 0; \
-    } while (0)
+    } while(0)
 
-    #define _ABS(registry) do { \
-        _INT _T = (registry); \
-        _INT _RES = (_T < 0) ? -_T : _T; \
+    // ABS instruction
+    #define ABS(registry) do { \
+        int64_t _T = (registry); \
+        int64_t _RES = (_T < 0) ? -_T : _T; \
         (registry) = _RES; \
         \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
         _CF = (_T < 0); \
-        _OF = (_T == _INT32_MIN); \
+        _OF = (_T == INT64_MIN); \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = ((_T ^ _RES) & 0x10) != 0; \
-    } while (0)
+    } while(0)
 
-    #define _INC(registry) do { \
-        _INT _T = (registry); \
-        _INT _RES = _T + 1; \
+    // INC instruction
+    #define INC(registry) do { \
+        int64_t _T = (registry); \
+        int64_t _RES = _T + 1; \
         (registry) = _RES; \
         \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
-        _OF = (_T == _INT32_MAX); \
+        _OF = (_T == INT64_MAX); \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = (((_T ^ 1 ^ _RES) & 0x10) != 0); \
-    } while (0)
+    } while(0)
 
-    #define _DEC(registry) do { \
-        _INT _T = (registry); \
-        _INT _RES = _T - 1; \
+    // DEC instruction
+    #define DEC(registry) do { \
+        int64_t _T = (registry); \
+        int64_t _RES = _T - 1; \
         (registry) = _RES; \
         \
-        /* Update flags register */ \
         _ZF = (_RES == 0); \
         _SF = (_RES < 0); \
-        _OF = (_T == _INT32_MIN); \
+        _OF = (_T == INT64_MIN); \
         _PF = (__builtin_parity((unsigned)_RES) == 0); \
         _AF = (((_T ^ 1 ^ _RES) & 0x10) != 0); \
-    } while (0)
+    } while(0)
 
 #endif /* ARITHMETICS_H */
